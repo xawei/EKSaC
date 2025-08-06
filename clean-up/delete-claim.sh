0e5e29444c1a9c46bbff2aa941b3feeb4cd120c6
xawei <csxawei@foxmail.com>
2025-08-07 00:17:55 +0800

    Update deletion script

--- clean-up/delete-claim.sh
+++ clean-up/delete-claim.sh
@@ -1,4 +1,4 @@
 # switch to control plane cluster and execute
-kubectl delete basecomponent.consumable.trustbank.sg/andy-cluster-base-components
-kubectl delete eksclusterkcl.consumable.trustbank.sg/andy-cluster
-kubectl delete networkkcl.consumable.trustbank.sg/andy-network
+kubectl delete basecomponent.consumable.trustbank.sg/andy-cluster-base-components -n eksac
+kubectl delete eksclusterkcl.consumable.trustbank.sg/andy-cluster -n eksac
+kubectl delete networkkcl.consumable.trustbank.sg/andy-network -n eksac
